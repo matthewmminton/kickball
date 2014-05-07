c3509dde916d01bd0953512e6577b54e9be8aa21
Matthew <matthew.minton@gmail.com>
2014-05-06 21:10:22 -0400

    position_profile.erb added

--- server.rb
+++ server.rb
@@ -2,10 +2,16 @@ require 'sinatra'
 
 get '/' do
   @teams = ["Simpson Slammers", "Jetson Jets", "Flinestone Fire", "Griffin Goats"]
+  @positions = ["Catcher", "Pitcher", "1st Base", "2nd Base", "3rd Base", "Shortstop", "Right Field", "Center Field", "Left Field"]
   erb :index
 end
 
 get '/teams/:team_name' do
   @team = params[:team_name]
   erb :team_profile
+end
+
+get '/positions/:position_name' do
+  @position = params[:position_name]
+  erb :position_profile
 end
